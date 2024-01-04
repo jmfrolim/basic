b8c3669b34285481d0b72761578c02aa98dccd0e
jmfrolim <joao.mfreitas76@gmail.com>
2024-01-04 14:27:03 -0300

    Adicionando array

--- theBasic.rb
+++ theBasic.rb
@@ -1,42 +1,57 @@
 def da_boas_vindas
   puts "Bem-vindo ao jogo da adivinhação"
   puts "Qual é o seu nome?"
-  nome = gets
+  nome = gets.strip
   puts "\n\n\n\n\n\n"
-  puts "Começaremos o jogo para você, " + nome
+  puts "Começaremos o jogo para você, #{nome} "
 end
 
 def sorteia_numero_secreto
+  sorteado = 175
   puts "Escolhendo um número secreto entre 0 e 200..."
   puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-  sorteado = 175
+  sorteado
+end
+
+def pede_um_numero(chutes,tentativa,limite_de_tentativas)
+  puts "\n\n\n\n"
+  puts "Tentativa #{tentativa} de #{limite_de_tentativas}"
+  puts "Chutes até agora: #{chutes}"
+  puts "Entre com o número"
+  chute = gets.strip
+  puts "Será que acertou? Você chutou  #{chute} "
+  chute.to_i
+end
+
+def verifica_se_acertou(numero_secreto, chute)
+  acertou = numero_secreto == chute
+  if acertou
+    puts  "Acertou miseravi!"
+    return true
+  end
+
+  maior = numero_secreto > chute
+  if maior
+    puts "O número secreto é maior!"
+  else
+    puts "O número secreto é menor!"
+  end
+  return false
 end
 
 da_boas_vindas()
 numero_secreto = sorteia_numero_secreto()
 
-
 tentativa = 0
-limite_de_tentativas = 3
+limite_de_tentativas = 5
+chutes = []
+
 for tentativa in 1..limite_de_tentativas
-  puts "\n\n\n\n"
-  puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
-  puts "Entre com o número"
-  chute = gets
-  puts "Será que acertou? Você chutou " + chute
-  acertou = 175 == chute.to_i
+  chute = pede_um_numero(chutes, tentativa, limite_de_tentativas )
 
-  if acertou
-    puts  "Acertou miseravi"
+  chutes << chute
+
+  if verifica_se_acertou(numero_secreto, chute)
     break
-  else
-    puts  "Errou"
-    maior = numero_secreto > chute.to_i
-    if maior
-      puts "O número secreto é maior!"
-    else
-      puts "O número secreto é menor!"
-    end
   end
-
 end
